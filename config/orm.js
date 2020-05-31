99f2e1a93d11f66e034fb6f5c172b5c515c44e2f
Kaeneth Dayao <kendayao@gmail.com>
2020-05-31 10:31:36 -0700

    connetion.js and orm.js setup

---
+++ config/orm.js
@@ -0,0 +1,29 @@
+var connection = require("../config/connection.js");
+
+
+var orm={
+
+    selectAll: function(table, cb){
+    var query= "SELECT * FROM " + table + ";"
+        connection.query(query,function(err,data){
+            if (err) throw err;
+            cb(result)
+        });
+
+    },
+
+    insertOne: function(table, cols, vals, cb){
+        
+
+    },
+
+    updateOne: function(table, cols, conition, cb){
+
+
+    },
+
+}
+
+
+
+module.exports=orm;
