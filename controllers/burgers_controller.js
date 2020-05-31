e607256aac8e8869bbe47841bfdbb6e7609c0929
Kaeneth Dayao <kendayao@gmail.com>
2020-05-31 10:42:05 -0700

    controller.js setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,36 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+
+router.get
+router.post
+
+router.put
+
+// if (result.changedRows == 0) {
+//     // If no rows were changed, then the ID must not exist, so 404
+//     return res.status(404).end();
+//   } else {
+//     res.status(200).end();
+//   }
+
+
+router.delete
+
+// if (result.affectedRows == 0) {
+//     // If no rows were changed, then the ID must not exist, so 404
+//     return res.status(404).end();
+//   } else {
+//     res.status(200).end();
+//   }
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
