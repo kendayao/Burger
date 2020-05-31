1d7ca10d7e85cd2773fe1beef7cd1965a5d43361
Kaeneth Dayao <kendayao@gmail.com>
2020-05-31 10:14:21 -0700

    added db folder, schema and sseds files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Western Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", true);
